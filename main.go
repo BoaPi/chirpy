48579f5d93087a218dee8e76fce124bcabfe57cd
boapi <tobias.weyer1205@gmail.com>
2025-02-03 14:14:15 +0100

    refactor with insights from offical solution

--- main.go
+++ main.go
@@ -25,5 +25,5 @@ func main() {
 func handlerReadniness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
