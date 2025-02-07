93ef4064e9012d9f58ca513c8db8bcbbd3fd0248
Tobias Weyer <tobias.weyer1205@gmail.com>
2025-02-07 14:53:19 +0100

    Update handler_chirps_get.go

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"net/http"
+	"slices"
+	"strings"
 
+	"github.com/BoaPi/chirpy/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetAllChirps(r.Context())
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		parsedAuthorID, err := uuid.Parse(authorIDString)
+		if err != nil {
+			responseWithError(w, http.StatusInternalServerError, "couldn't parse author id", err)
+			return
+		}
+		authorID = parsedAuthorID
+	}
+
+	sortDirection := "asc"
+	sortDirectionString := r.URL.Query().Get("sort")
+	if sortDirectionString == "desc" {
+		sortDirection = sortDirectionString
+	}
+
+	dbChirps, err := cfg.db.GetChirps(r.Context(), authorID)
 	if err != nil {
-		responseWithError(w, http.StatusInternalServerError, "Couldn't fetch chirps", err)
+		responseWithError(w, http.StatusInternalServerError, "couldn't fetch chirps", err)
 		return
+	}
+
+	if sortDirection == "desc" {
+		slices.SortFunc(dbChirps, func(a database.Chirp, b database.Chirp) int {
+			if a.CreatedAt.Before(b.CreatedAt) {
+				return 1
+			} else if a.CreatedAt.After(b.CreatedAt) {
+				return -1
+			}
+			return 0
+		})
 	}
 
 	chirps := []Chirp{}
